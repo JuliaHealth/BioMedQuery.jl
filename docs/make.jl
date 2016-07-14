7a80e3b6416523e564db52ccd06ef3e1a90396da
Isa Restrepo <isabelrestre@gmail.com>
2016-07-14 10:10:06 -0400

    Missing material theme dependency

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, BioMedQuery
 makedocs()
 
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),
     repo = "github.com/bcbi/BioMedQuery.jl.git",
     julia  = "0.4",
     osname = "osx"
