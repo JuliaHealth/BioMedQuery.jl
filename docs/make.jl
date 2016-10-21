8e65d51158686742b7f52220e2d3b2266c934174
Maria Isabel Restrepo <isabelrestre@gmail.com>
2016-10-21 11:51:13 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,6 @@ makedocs()
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),
     repo = "github.com/bcbi/BioMedQuery.jl.git",
-    julia  = "0.4",
+    julia  = "0.5",
     osname = "osx"
 )
