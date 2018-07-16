9d5d6a51abe2c40a39fc7ef18e008014b385ddd1
Maria Isabel Restrepo <isabelrestre@gmail.com>
2018-07-16 14:08:55 -0400

    dummy test to trigger travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,25 +12,27 @@ using MySQL
 using SQLite
 using DataStreams
 
-my_tests = [
-            # ("dbutils_sqlite.jl",   "       Testing: DBUtils SQLite"),
-            # ("dbutils_mysql.jl",    "       Testing: DBUtils MySQL"),
-            # ("pubmed.jl",           "       Testing: Eutils/PubMed"),
-            # ("export_citations.jl", "       Testing: Export Citations"),
-            # ("pubmed_parse.jl",     "       Testing: Entrez Parsing"),
-            # ("ct.jl",               "       Testing: CLINICAL TRIALS"),
-            # ("processes_mysql.jl",  "       Testing: Processes MySQL"),
-            # ("processes_sqlite.jl", "       Testing: Processes SQLite")
-            ]
+@test 1==1
 
-println("Running tests:")
+# my_tests = [
+#             ("dbutils_sqlite.jl",   "       Testing: DBUtils SQLite"),
+#             ("dbutils_mysql.jl",    "       Testing: DBUtils MySQL"),
+#             ("pubmed.jl",           "       Testing: Eutils/PubMed"),
+#             ("export_citations.jl", "       Testing: Export Citations"),
+#             ("pubmed_parse.jl",     "       Testing: Entrez Parsing"),
+#             ("ct.jl",               "       Testing: CLINICAL TRIALS"),
+#             ("processes_mysql.jl",  "       Testing: Processes MySQL"),
+#             ("processes_sqlite.jl", "       Testing: Processes SQLite")
+#             ]
 
-for (my_test, test_string) in my_tests
-    println("-----------------------------------------")
-    println("-----------------------------------------")
-    println(test_string)
-    println("-----------------------------------------")
-    println("-----------------------------------------")
+# println("Running tests:")
 
-    include(my_test)
-end
+# for (my_test, test_string) in my_tests
+#     println("-----------------------------------------")
+#     println("-----------------------------------------")
+#     println(test_string)
+#     println("-----------------------------------------")
+#     println("-----------------------------------------")
+
+#     include(my_test)
+# end
