c6129425d4db5fd0fa0edb713a65a426ebbdf445
Isabel Restrepo <isabelrestre@gmail.com>
2018-11-14 15:10:56 -0500

    swtich deploy os to linux, set documenter flag

--- docs/make.jl
+++ docs/make.jl
@@ -4,13 +4,13 @@ using Literate
 # compile all examples in BioMedQuery/examples/literate_src into markdown and jupyter notebooks for documentation
 for (root, dirs, files) in walkdir("examples/literate_src")
     for file in files
-        Literate.notebook(joinpath(root,file), joinpath(@__DIR__, "src", "notebooks"), execute=false)
+        Literate.notebook(joinpath(root,file), joinpath(@__DIR__, "src", "notebooks"), execute=false, documenter=true)
     end
 end
 
 for (root, dirs, files) in walkdir("examples/literate_src")
     for file in files
-        Literate.markdown(joinpath(root,file), joinpath(@__DIR__, "src", "examples"))
+        Literate.markdown(joinpath(root,file), joinpath(@__DIR__, "src", "examples"), documenter=true)
     end
 end
 
@@ -20,5 +20,5 @@ deploydocs(
     deps   = Deps.pip("mkdocs==0.17.5", "mkdocs-material==2.9.4"),
     repo = "github.com/bcbi/BioMedQuery.jl.git",
     julia  = "0.7",
-    osname = "osx"
+    osname = "linux"
 )
