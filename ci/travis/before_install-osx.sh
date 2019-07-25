7974911863ff8c104c66d0ebd75833372b483a0c
Ian <ian_bacher@brown.edu>
2019-07-25 09:07:03 -0400

    fix: a different way

--- ci/travis/before_install-osx.sh
+++ ci/travis/before_install-osx.sh
@@ -8,5 +8,12 @@ echo "------------------------------------------"
 echo ">>> brew install mariadb"
 brew update
 brew install mariadb
+
+echo ">>> configure mariadb"
+echo "[mysqld]" > $HOME/.my.cnf
+echo "secure_file_priv = \"\" ">> $HOME/.my.cnf
+echo "default_authentication_plugin = mysql_native_password" >> $HOME/.my.cnf
+echo "local_infile = 1" >> $HOME/.my.cnf
+
 echo ">>> mysql.server start"
 mysql.server start
