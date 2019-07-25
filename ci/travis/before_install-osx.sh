2f4879b176d01d3092f64807f0e5d660fb347c75
Ian <ian_bacher@brown.edu>
2019-07-25 09:59:37 -0400

    fix: attempt another fix of travis

--- ci/travis/before_install-osx.sh
+++ ci/travis/before_install-osx.sh
@@ -11,6 +11,7 @@ brew install mariadb
 brew link mariadb
 
 echo ">>> configure mariadb"
+mkdir -p /usr/local/etc/my.cnf.d
 echo "[mysqld]" > $HOME/.my.cnf
 echo "secure_file_priv = \"\" ">> $HOME/.my.cnf
 echo "default_authentication_plugin = mysql_native_password" >> $HOME/.my.cnf
